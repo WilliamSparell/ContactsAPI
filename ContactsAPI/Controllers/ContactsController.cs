98ae9b85ea57158a231bec7b9690dd972ecf73d0
WilliamSparell <williamatlasnova@gmail.com>
2022-08-30 11:43:46 +0200

    Add project files.

---
+++ ContactsAPI/Controllers/ContactsController.cs
@@ -0,0 +1,40 @@
+﻿using ContactsAPI.Data;
+using ContactsAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactsController : Controller
+    {
+        private readonly ContactsAPIDbContext dbContext;
+
+        public ContactsController(ContactsAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetContacts()
+        {
+            return Ok(dbContext.Contacts.ToList());   
+        }
+
+        [HttpPost]
+        public IActionResult AddContact(AddContactRequest addContactRequest)
+        {
+            var contact = new Contact()
+            {
+                Id = Guid.NewGuid(),
+                FullName = addContactRequest.FullName,
+                Email = addContactRequest.Email,
+                Phone = addContactRequest.Phone,
+                Address = addContactRequest.Address
+            };
+            dbContext.Contacts.Add(contact);
+            dbContext.SaveChanges();
+            return Ok(contact);
+        }
+    }
+}
